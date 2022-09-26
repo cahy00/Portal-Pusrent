c182e65a0c91b56d8f7988602e8fee5002566bd1
cahy00 <cariercahyo@gmail.com>
2022-09-26 16:48:19 +0800

    #

--- resources/views/home.blade.php
+++ resources/views/home.blade.php
@@ -48,53 +48,7 @@
   </section><!-- End Hero -->
 
   <main id="main">
-
-    <!-- ======= About Section ======= -->
-    <section id="about" class="about">
-      @include('about')
-    </section><!-- End About Section -->
-
-    <!-- ======= About Video Section ======= -->
-    <section id="about-video" class="about-video">
-      @include('about-video')
-    </section><!-- End About Video Section -->
-
-    <!-- ======= Clients Section ======= -->
-    
-		<!-- End Clients Section -->
-
-    <!-- ======= Testimonials Section ======= -->
-    
-		<!-- End Testimonials Section -->
-
-    <!-- ======= Services Section ======= -->
-    <section id="services" class="services section-bg">
-			@include('services')
-    </section><!-- End Sevices Section -->
-
-    <!-- ======= Cta Section ======= -->
-  <!-- End Cta Section -->
-
-    <!-- ======= Portfolio Section ======= -->
-    <section id="portfolio" class="portfolio">
-      @include('portfolio')
-    </section><!-- End Portfolio Section -->
-
-    <!-- ======= Team Section ======= -->
-    <section id="team" class="team section-bg">
-      @include('team')
-    </section><!-- End Team Section -->
-
-    <!-- ======= Frequently Asked Questions Section ======= -->
-    <section id="faq" class="faq section-bg">
-      @include('faq')
-    </section><!-- End Frequently Asked Questions Section -->
-
-    <!-- ======= Contact Section ======= -->
-    <section id="contact" class="contact">
-      @include('contact')
-    </section><!-- End Contact Section -->
-
+		@yield('main')
   </main><!-- End #main -->
 
   <!-- ======= Footer ======= -->
--- routes/web.php
+++ routes/web.php
@@ -13,6 +13,14 @@ use Illuminate\Support\Facades\Route;
 |
 */
 
-Route::get('/', function () {
+Route::get('/home', function () {
     return view('home');
+});
+
+Route::get('/', function () {
+	return view('body');
+});
+
+Route::get('/anjab', function () {
+	return view('service.anjab');
 });
